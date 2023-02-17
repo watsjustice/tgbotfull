2989e0fd250869a098f4bbcf4222cb49c7629c4d
root <root@vm6.template>
2023-02-17 06:37:24 +0300

    realise

---
+++ src/entities/last_user_order_entity.ts
@@ -0,0 +1,39 @@
+import Knex from "knex";
+import knexConfig from "../db/knexfile";
+import {Model} from "objection";
+import { Order } from "./order_entity";
+
+Model.knex(Knex(knexConfig));
+export class LastOrder extends Model {
+
+    static get tableName() {
+        return 'lastOrder'
+    }
+
+    static relationMappings() {
+
+        return {
+            order : {
+                relation : Model.HasManyRelation,
+                modelClass : Order,
+                join : {
+                    from : "order.id",
+                    to: "lastOrder.orderId"
+                }
+            }
+        }
+    }
+
+    orderId: number;
+
+    userId : string;
+
+    status: boolean;
+
+    order : Order;
+
+    price : number;
+
+
+
+}   
